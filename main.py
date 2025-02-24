985499442950089548ae62f7116b22c2ac3aa1c4
burlingk <cagox@cagox.net>
2025-02-24 19:44:40 +0900

    Initial firing code in place.
    Can create a shot and fire it. Some cleanup needed

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 updateable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
 Player.containers = (updateable, drawable)
 Asteroid.containers = (asteroids, updateable, drawable)
 AsteroidField.containers = (updateable,)
+Shot.containers = (updateable, shots, drawable)
 
 def main():
     pygame.init()
